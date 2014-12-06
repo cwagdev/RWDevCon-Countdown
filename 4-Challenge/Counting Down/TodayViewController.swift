4ffb89d56bee0518dcda8ea93d902622e6936364
Chris Wagner <cwagner118@gmail.com>
2014-12-06 15:00:33 -0700

    Expanding cells for widget

--- 4-Challenge/Counting Down/TodayViewController.swift
+++ 4-Challenge/Counting Down/TodayViewController.swift
@@ -1,43 +1,61 @@
-//
-//  TodayViewController.swift
-//  Counting Down
-//
-//  Created by Chris Wagner on 12/6/14.
-//  Copyright (c) 2014 Ray Wenderlich LLC. All rights reserved.
-//
+/*
+* Copyright (c) 2014 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 import UIKit
 import NotificationCenter
 import CountdownKit
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
-      preferredContentSize = CGSize(width: 0, height: 150)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
-        // Perform any setup necessary in order to update the view.
-
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-
-        completionHandler(NCUpdateResult.NewData)
-    }
-    
+  
+  private var expandedIndexPath: NSIndexPath?
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    preferredContentSize = CGSize(width: 0, height: 150)
+  }
+  
+  func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+    tableView.reloadData()
+    completionHandler(NCUpdateResult.NewData)
+  }
+  
 }
 
 extension TodayViewController: UITableViewDataSource, UITableViewDelegate {
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return CoreDataController.sharedInstance.targetDays.count
+    let rowCount = CoreDataController.sharedInstance.targetDays.count
+    
+    return rowCount
+  }
+  
+  func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    if let expandedIndexPath = expandedIndexPath {
+      if indexPath.compare(expandedIndexPath) == .OrderedSame {
+        return 150
+      }
+    }
+    
+    return 50
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -46,5 +64,28 @@ extension TodayViewController: UITableViewDataSource, UITableViewDelegate {
     cell.targetDay = targetDay
     
     return cell
+  }
+  
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    let cell = tableView.cellForRowAtIndexPath(indexPath) as TargetDayCell
+    if let expandedIndexPath = expandedIndexPath {
+      if indexPath.compare(expandedIndexPath) == .OrderedSame {
+        self.expandedIndexPath = nil
+      } else {
+        self.expandedIndexPath = indexPath
+      }
+    } else {
+      self.expandedIndexPath = indexPath
+    }
+    
+    tableView.beginUpdates()
+    tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    tableView.endUpdates()
+    if expandedIndexPath == nil {
+      tableView.setContentOffset(CGPointZero, animated: true)
+    } else {
+      tableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: .Top, animated: true)
+    }
+    
   }
 }
